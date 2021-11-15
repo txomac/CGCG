6a72b63b6975d0ff9766331aa48a134ad9b813b9
Thomas  Henaff <thomas.henaff@epsi.fr>
2021-11-15 08:50:09 +0100

    first commit

---
+++ CGCG/Reference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGCG
+{
+    class Reference
+    {
+        public string reference { get; set; }
+        public string marque { get; set; }
+        public string libelle { get; set; }
+        public int desactive { get; set; }
+
+    }
+}
