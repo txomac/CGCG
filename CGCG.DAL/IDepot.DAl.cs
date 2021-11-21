2b3ac36e3d01d55753343ba2100612f3abd88cba
Thomas  Henaff <thomas.henaff@epsi.fr>
2021-11-21 10:45:24 +0100

    add .DAL

--- CGCG.DAL/IDepot.DAl.cs
+++ CGCG.DAL/IDepot.DAl.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CGCG.DAL
 {
-    public class Class1
+    interface IDepot_DAL<Type_DAL>
     {
+        public string ChaineDeConnexion { get; set; }
+
+        public List<Type_DAL> GetAll();
+
+        public Type_DAL GetByID(int ID);
+
+        public Type_DAL Insert(Type_DAL item);
+
+        public Type_DAL Update(Type_DAL item);
+
+        public void Delete(Type_DAL item);
     }
 }
