517caa6c14b1ff72e314347ab5792c726181993a
Enzo <enzo.gouerec@epsi.fr>
2022-02-01 22:16:23 +0100

    fournisseur_reference

---
+++ CGCG.API/Controllers/Fournisseur_ReferenceController.cs
@@ -0,0 +1,42 @@
+﻿using CGCG.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGCG.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+   
+        public class Fournisseur_ReferenceController : ControllerBase
+        {
+            private IFournisseurs_ReferencesService service;
+
+            public Fournisseur_ReferenceController(IFournisseurs_ReferencesService srv)
+            {
+                service = srv;
+            }
+
+            [HttpGet]
+            public IEnumerable<Fournisseur_Reference_DTO> GetAllReference()
+            {
+                return service.GetAllFournisseursReferences().Select(fr => new Fournisseur_Reference_DTO()
+                {
+                    id_fournisseurs = fr.id_fournisseurs,
+                    id_references = fr.id_references
+                    
+                });
+            }
+
+            [HttpPost]
+        public Fournisseur_Reference_DTO Insert(Fournisseur_Reference_DTO fr)
+        {
+            var fr_metier = service.Insert(new Fournisseurs_References (fr.id_fournisseurs, fr.id_references));
+            //je renvoie l'objet DTO
+            return fr;
+        }
+    }
+}
---
+++ CGCG.API/Controllers/ReferenceController.cs
@@ -0,0 +1,47 @@
+﻿using CGCG.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGCG.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+   
+        public class ReferenceController : ControllerBase
+        {
+            private IReferencesService service;
+
+            public ReferenceController(IReferencesService srv)
+            {
+                service = srv;
+            }
+
+            [HttpGet]
+            public IEnumerable<Reference_DTO> GetAllReference()
+            {
+                return service.GetAllReferences().Select(r => new Reference_DTO()
+                {
+                    id = r.id,
+                    reference = r.reference,
+                    libelle = r.libelle,
+                    marque = r.marque,
+                    desactive = r.desactive,
+                    id_fournisseurs = r.id_fournisseurs,
+                });
+            }
+
+            [HttpPost]
+        public Reference_DTO Insert(Reference_DTO r)
+        {
+            var r_metier = service.Insert(new References(r.id, r.reference, r.libelle, r.marque, r.desactive, r.id_fournisseurs));
+            //Je récupère l'ID
+            r.id = r_metier.id;
+            //je renvoie l'objet DT
+            return r;
+        }
+    }
+}
